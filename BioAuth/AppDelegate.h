e7cdeacb3eb71b8bc493679e8eda5e1716b4e1c6
Alberto Plata <alberto.plata@alberto-plata.attlocal.net>
2017-05-01 20:48:15 -0700

    new project after removed spaces

---
+++ BioAuth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BioAuth
+//
+//  Created by Alberto Plata on 5/1/17.
+//  Copyright © 2017 jediHKRZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BioAuth/Source/JHKRZSignOnUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  JHKRZSignOnUIViewController.h
+//  BioAuth
+//
+//  Created by Alberto Plata on 5/1/17.
+//  Copyright © 2017 jediHKRZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JHKRZSignOnUIViewController : UIViewController
+
+@end
